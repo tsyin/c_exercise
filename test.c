ce75e3ccc81a5bb3c9ce2d30aa59e392aa6f6961
tsyin_desktop_mobi <1115653102@qq.com>
2021-11-25 15:53:31 +0800

    1125小试

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int imate =2;
+    long shot = 53456;
+    char grade = "A";
+    float log =2.71828;
+    printf("The odds against the %d were %ld to 1.\n",imate,shot);
+    printf("A score of %c is not an %f reade.\n",log,grade);
+    
+    return 0;
+}
