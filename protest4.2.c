6546d9be3faffea338cff8c776bc5199e9aab35c
tsyin_desktop_mobi <1115653102@qq.com>
2021-12-01 17:00:49 +0800

    ss

--- protest4.2.c
+++ protest4.2.c
@@ -1,14 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-    const float PI = 3.1415926;
-    float area,circum,radius;
-    printf("What is the radius of the pizza?\n");
-    scanf("%f",&radius);
-    area = PI*radius*radius;
-    circum = 2.0*PI*radius;
-    printf("your basic pizza parameters are as follows:\n");
-    printf("circumference = %1.2f,area = %1.2f\n",circum,area);
+int main(void)
+
+{
+    int i = 5;
+    while(i++<10)
+        printf("%d",i);
     return 0;
-
 }
